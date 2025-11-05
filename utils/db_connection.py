bfc2e6fa7c373e779e9922c371db7fd5514cf555
matboe-94 <matthias.boeker@digdir.no>
2025-11-05 10:57:53 +0100

    add empty row

--- utils/db_connection.py
+++ utils/db_connection.py
@@ -1,6 +1,7 @@
 from typing import Any
 import struct
 import urllib
+import uuid
 from sqlmodel import Session, select
 from sqlalchemy import create_engine, event, Engine
 from sqlalchemy.exc import OperationalError
@@ -9,26 +10,23 @@ from tenacity import retry, retry_if_exception_type, wait_exponential, stop_afte
 from dotenv import load_dotenv
 from dataclasses import dataclass
 
-from .project_loader import PortfolioProject, Fremskritt, Resursbehov, Samarabeid, Problemstilling, Tiltak, Risikovurdering, Malbilde, DigitaliseringStrategi, ProjectData, convert_list, apply_changes
-
+from utils.data_models import PortfolioProject, PortfolioProjectUI, Fremskritt, FremskrittUI, Resursbehov, ResursbehovUI, Samarabeid, SamarabeidUI, Problemstilling, ProblemstillingUI, Tiltak, TiltakUI, Risikovurdering, RisikovurderingUI, Malbilde,  MalbildeUI, DigitaliseringStrategi, DigitaliseringStrategiUI
 load_dotenv()
 
 @dataclass
 class ProjectData:
-
-    fremskritt: Fremskritt
-    samarabeid: Samarabeid
-    portfolioproject: PortfolioProject
-    problemstilling: Problemstilling
-    tiltak: Tiltak
-    risikovurdering: Risikovurdering
-    malbilde: Malbilde
-    resursbehov: Resursbehov
-    digitaliseringstrategi: DigitaliseringStrategi
+    fremskritt: FremskrittUI | None
+    samarabeid: SamarabeidUI | None
+    portfolioproject: PortfolioProjectUI | None
+    problemstilling: ProblemstillingUI | None
+    tiltak: TiltakUI | None
+    risikovurdering: RisikovurderingUI | None
+    malbilde: MalbildeUI | None
+    resursbehov: ResursbehovUI | None
+    digitaliseringstrategi: DigitaliseringStrategiUI | None
 
 
 def get_single_project_data(project_id: str, sql_models: dict):
-    
     statement_dict = {}
     for schema_name, schema in sql_models.items():
         statement_dict[schema_name] = select(schema).where(schema.prosjekt_id == project_id and schema.er_gjeldende == True)
@@ -37,11 +35,13 @@ def get_single_project_data(project_id: str, sql_models: dict):
 
 class DBConnector:
     engine: Engine
-    #{"fremskritt": Fremskritt, "samarabeid": Samarabeid, "portfolioproject": PortfolioProject, "problemstilling": Problemstilling, "tiltak": Tiltak, "risikovurdering": Risikovurdering, "malbilde": Malbilde, "resursbehov": Resursbehov, "digitaliseringstrategi": DigitaliseringStrategi}
+    sql_model_names: dict
 
-    def __init__(self, engine: Engine, sql_model_names: dict[str, Any]):
+    def __init__(self, engine: Engine, sql_models: dict[str, Any], ui_models: dict[str, Any]):
         self.engine = engine
-        self.sql_model_names = sql_model_names
+        self.sql_models = sql_models
+        self.ui_models = ui_models
+
 
     @classmethod
     def create_engine(cls, driver_name: str, server_name: str, database_name: str, fabric_client_id: str, fabric_tenant_id: str, fabric_client_secret: str):
@@ -65,9 +65,21 @@ class DBConnector:
             token_struct = struct.pack(f"<I{len(token_bytes)}s", len(token_bytes), token_bytes)
             SQL_COPT_SS_ACCESS_TOKEN = 1256
             cparams["attrs_before"] = {SQL_COPT_SS_ACCESS_TOKEN: token_struct}
-        sql_model_names = {"fremskritt": Fremskritt, "samarabeid": Samarabeid, "portfolioproject": PortfolioProject, "problemstilling": Problemstilling, "tiltak": Tiltak, "risikovurdering": Risikovurdering, "malbilde": Malbilde, "resursbehov": Resursbehov, "digitaliseringstrategi": DigitaliseringStrategi}
-        return cls(engine, sql_model_names)
+        sql_models = {"fremskritt": Fremskritt, "samarabeid": Samarabeid, "portfolioproject": PortfolioProject, "problemstilling": Problemstilling, "tiltak": Tiltak, "risikovurdering": Risikovurdering, "malbilde": Malbilde, "resursbehov": Resursbehov, "digitaliseringstrategi": DigitaliseringStrategi}
+        ui_models = {"fremskritt": FremskrittUI, "samarabeid": SamarabeidUI, "portfolioproject": PortfolioProjectUI, "problemstilling": ProblemstillingUI, "tiltak": TiltakUI, "risikovurdering": RisikovurderingUI, "malbilde": MalbildeUI, "resursbehov": ResursbehovUI, "digitaliseringstrategi": DigitaliseringStrategiUI}
+        return cls(engine, sql_models, ui_models)
     
+    def create_empty_project(self, email, user_name, prosjekt_id):#email,user_name=user_name, pid=prosjekt_id
+        empty_populated_schemas = {}
+        dict_of_schemas = self.ui_models
+        dict_of_schemas.pop("portfolioproject")
+        print(self.ui_models)
+        print(dict_of_schemas)
+        for name, schema in dict_of_schemas.items():
+            empty_populated_schemas[name] = schema(prosjekt_id=prosjekt_id, endret_av=user_name)
+        empty_populated_schemas["portfolioproject"] = PortfolioProject(epost_kontakt=email, prosjekt_id=prosjekt_id, endret_av=user_name)
+        return ProjectData(**empty_populated_schemas)
+
     @retry(retry=retry_if_exception_type(OperationalError),
     wait=wait_exponential(multiplier=1, min=1, max=10),
     stop=stop_after_attempt(3),
@@ -105,15 +117,26 @@ class DBConnector:
     def get_single_project(self, project_id: str):
         sql_model_dict = {}
         with Session(self.engine) as session:
-            stmt_dict = get_single_project_data(project_id, self.sql_model_names)
+            stmt_dict = get_single_project_data(project_id, self.sql_models)
             for sql_model_name, sql_statement in stmt_dict.items():
                 result = session.exec(sql_statement).first()
-                sql_model_dict[sql_model_name] = result
-        
+                if result:
+                    sql_model_dict[sql_model_name] = result
+                else: 
+                    sql_model_dict[sql_model_name] = self.sql_models[sql_model_name](prosjekt_id=project_id)
         project_data = ProjectData(
-                **sql_model_dict
+            fremskritt=FremskrittUI(**sql_model_dict["fremskritt"].dict()),
+            resursbehov=ResursbehovUI(**sql_model_dict["resursbehov"].dict()),
+            samarabeid=SamarabeidUI(**sql_model_dict["samarabeid"].dict()),
+            portfolioproject=PortfolioProjectUI(**sql_model_dict["portfolioproject"].dict()),
+            tiltak=TiltakUI(**sql_model_dict["tiltak"].dict()),
+            problemstilling=ProblemstillingUI(**sql_model_dict["problemstilling"].dict()),
+            risikovurdering=RisikovurderingUI(**sql_model_dict["risikovurdering"].dict()),
+            malbilde=MalbildeUI(**sql_model_dict["malbilde"].dict()),
+            digitaliseringstrategi=DigitaliseringStrategiUI(**sql_model_dict["digitaliseringstrategi"].dict()),
         )
         return project_data
+    
     @retry(retry=retry_if_exception_type(OperationalError),
     wait=wait_exponential(multiplier=1, min=1, max=10),
     stop=stop_after_attempt(3),
