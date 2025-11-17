614912e2559c8fa4222bccbe61ee7ded9bbaa24f
matboe-94 <matthias.boeker@digdir.no>
2025-11-17 13:49:46 +0100

    add int converter

--- utils/validators.py
+++ utils/validators.py
@@ -1,7 +1,16 @@
 import datetime
+from typing import Union
 
-def to_datetime(value):
+def to_datetime(value: str):
     if isinstance(value, str):
         return datetime.strptime(value,"%Y-%m-%d")
     else:
         return value
+
+def convert_to_int(number: Union[str, int, None]):
+    if isinstance(number, str):
+        return int(number)
+    if isinstance(number, int):
+        return number
+    return None
+    
