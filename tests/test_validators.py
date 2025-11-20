532ee7640acca50a46e57ed21209fe13a9e54b5f
matboe-94 <matthias.boeker@digdir.no>
2025-11-20 11:31:30 +0100

    add converting test

---
+++ tests/test_validators.py
@@ -0,0 +1,23 @@
+import pytest
+from utils.validators import validate_budget_distribution, turn_none_to_zero
+
+
+test_converting_years = [(50, 50), (None, 0)]
+
+@pytest.mark.parametrize("year_input, expected", test_converting_years)
+def test_turn_none_to_zero(year_input, expected):
+    assert expected == turn_none_to_zero(year_input)
+
+
+
+test_budget = [
+    (100, 50, 25, 25, False),
+    (100, 50, 25, 1, True),
+    (5, 50, 25, 25, True),
+    (100, 50, "25", 25, False),
+    (100, 50, None, 25, True),
+]
+
+@pytest.mark.parametrize("total, year_1, year_2, year_3, result", test_budget)
+def test_validate_budget_distribution(total, year_1, year_2, year_3, result):
+    assert result == validate_budget_distribution(total, year_1, year_2, year_3)
